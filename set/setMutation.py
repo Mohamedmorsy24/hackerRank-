b5327c89eb35e9b563274a67a472a639698d3d4b
yamany28 <yamany28@students.mans.edu.eg>
2020-07-28 17:42:47 +0200

    set mutation update

--- set/setMutation.py
+++ set/setMutation.py
@@ -3,13 +3,5 @@ N = set(map(int, input().split()))
 for i in range(int(input())):
     a = input().split()
     n = set(map(int, input().split()))
-    if a[0] == 'intersection_update':
-        N.intersection_update(n)
-    elif a[0] == 'update':
-        N.update(n)
-    elif a[0] == 'symmetric_difference_update':
-        N.symmetric_difference_update(n)
-    elif a[0] == 'difference_update':
-        N.difference_update(n)
-
+    eval('N.' + a[0] + '(n)')
 print(sum(N))
